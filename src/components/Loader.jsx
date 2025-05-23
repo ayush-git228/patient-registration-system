5e4a22d89b4af98f4461bd3928c01c4a2b4d436c
Ayush Gupta <ayushgupta228@gmail.com>
2025-05-23 09:36:03 +0530

    feat: to load before our application fetches db

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ export default function Loader({ message = "Loading..." }) {
   return (
     <div className="loader-container">
       <div className="loader-spinner" />
-      <div style={{ marginTop: 18, color: "#2461a6", fontWeight: 500 }}>{message}</div>
+      <div style = {{ marginTop: 18, color: "#2461a6", fontWeight: 500 }}>{message}</div>
     </div>
   );
 }
